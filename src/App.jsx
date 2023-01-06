c175e12b06d1fc0deaa497051059e3e4b1a084d6
hackpres <102390515+hackpres@users.noreply.github.com>
2023-01-06 12:38:18 -0500

    setState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParms from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet animal="Dog" name="Luna" breed="Havanese" />
-      <Pet animal="Bird" name="Pepper" breed="Cockatiel" />
-      <Pet animal="Cat" name="Doink" breed="Mixed"   />
+      <SearchParms />
     </div>
   )
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
