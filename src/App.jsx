3d5ad849b5b19d45d6af1260e5ccba60638df8aa
hackpres <102390515+hackpres@users.noreply.github.com>
2023-01-06 12:18:10 -0500

    Configuring ESLint & React

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <h1>Adopt Me!</h1>
       <Pet animal="Dog" name="Luna" breed="Havanese" />
       <Pet animal="Bird" name="Pepper" breed="Cockatiel" />
-      <Pet animal="Cat" name="Doink" breed="Mixed" />
+      <Pet animal="Cat" name="Doink" breed="Mixed"   />
     </div>
   )
 };
