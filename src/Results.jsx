85c0304bcf41f24bbf4c9a0ebef007e409c1f456
hackpres <102390515+hackpres@users.noreply.github.com>
2023-01-06 14:39:58 -0500

    Component Composition

---
+++ src/Results.jsx
@@ -0,0 +1,26 @@
+import Pet from './Pet';
+
+import React from 'react'
+
+function Results({ pets }) {
+    return (
+        <div className='search'>
+            {!pets.length ? (
+                <h1>No Pets Found</h1>
+            ) : (
+                pets.map(pet => (
+                    <Pet
+                        animal={pet.animal}
+                        name={pet.name}
+                        breed={pet.breed}
+                        images={pet.images}
+                        location={`${pet.city}, ${pet.state}`}
+                        key={pet.id}
+                    />
+                ))
+            )}
+        </div>
+    );
+};
+
+export default Results
