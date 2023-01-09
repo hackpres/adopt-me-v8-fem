475f1a97c6e695884dca463e7e2b7965a95f37b5
hackpres <102390515+hackpres@users.noreply.github.com>
2023-01-09 12:09:26 -0500

    Installing Tailwind CSS / Styling with Tailwind CSS

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -8,79 +8,72 @@ import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParms = () => {
-    const [requestParams, setRequestParams] = useState({
-        location: "",
-        animal: "",
-        breed: "",
-    });
-    const [animal, setAnimal] = useState("");
-    const [breeds] = useBreedList(animal);
-    // eslint-disable-next-line no-unused-vars
-    const [adoptedPet, _] = useContext(AdoptedPetContext);
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+  // eslint-disable-next-line no-unused-vars
+  const [adoptedPet, _] = useContext(AdoptedPetContext);
 
-    const results = useQuery(["search", requestParams], fetchSearch);
-    const pets = results?.data?.pets ?? [];
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
 
-    return (
-        <div className="search-params">
-            <form onSubmit={e => {
-                e.preventDefault();
-                const formData = new FormData(e.target);
-                const obj = {
-                    animal: formData.get("animal") ?? "",
-                    location: formData.get("location") ?? "",
-                    breed: formData.get("breed") ?? "",
-                };
-                setRequestParams(obj);
-            }}>
-                {
-                    adoptedPet ? (
-                        <div className="pet image-container">
-                            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
-                        </div>
-                    ) : null
-                }
-                <label htmlFor="location">
-                    Location
-                    <input
-                        name="location"
-                        id="location"
-                        placeholder="Location"
-                    />
-                </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select
-                        id="animal"
-                        value={animal}
-                        onChange={(e) => {
-                            setAnimal(e.target.value)
-                        }}
-                    >
-                        <option />
-                        {ANIMALS.map((animal) => (
-                            <option key={animal}>{animal}</option>
-                        ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select
-                        id="breed"
-                        disabled={breeds.length === 0}
-                        name="breed"
-                    >
-                        <option />
-                        {breeds.map((breed) => (
-                            <option key={breed}>{breed}</option>
-                        ))}
-                    </select>
-                </label>
-                <button>Submit</button>
-            </form>
-            <Results pets={pets} />
-        </div>
-    );
+  return (
+    <div className="my-0 mx-auto w-11/12">
+      <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            location: formData.get("location") ?? "",
+            breed: formData.get("breed") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
+        <label htmlFor="location">
+          Location
+          <input name="location" id="location" placeholder="Location" />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select id="breed" disabled={breeds.length === 0} name="breed">
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
 };
 
-export default SearchParms
+export default SearchParms;
